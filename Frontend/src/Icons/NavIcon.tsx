bd4daa0c3d3e9d538bfb06460a908f894d3d365f
Facundo Pettersson <visualstudio.creator23@gmail.com>
2025-02-10 17:48:13 -0300

    feat: Changes

--- Frontend/src/Icons/NavIcon.tsx
+++ Frontend/src/Icons/NavIcon.tsx
@@ -1,9 +1,9 @@
 
 
 export function NavIcon({ Icon }: { Icon: React.ElementType }) {
-    return (
-      <div className="p-2 text-white/90 hover:bg-white/10 rounded-lg cursor-pointer transition-colors">
-        <Icon size={24} />
-      </div>
-    );
-  }
+  return (
+    <div className="p-2 fill-white hover:bg-white/10 rounded-lg cursor-pointer transition-colors">
+      <Icon size={24} color="#fff" strokeWidth="2.4" />
+    </div>
+  );
+}
